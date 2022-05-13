bac4dc45672e328620587358e16c16beb15c8c6f
ninjachen <nyto9999@gmail.com>
2022-05-13 19:14:11 +0800

    Initial Commit

---
+++ faceMaskData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  faceMaskData
+//
+//  Created by 宇宣 Chen on 2022/5/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
